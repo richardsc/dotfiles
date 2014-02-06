c68b8661f402fe7a9041ba81cd345053ae7b840a
Clark Richards <clark.richards@gmail.com>
2014-02-05 20:50:27 -0500

    fix custom.el copy error

--- custom.el
+++ custom.el
@@ -1,6 +1,24 @@
-;; This is so that I can keep GNU emacs and Aquamacs separate. Check
-;; to see if it's Aqaumacs calling, and if so, ignore everything
-;; (Aquamacs preferences and customizations are in
-;; `~/Library/Preferences/Aquamacs Emacs/`)
-(unless (boundp 'aquamacs-version)
-  (load-file "~/.emacs-gnu.el"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex -synctex=1")
+ '(TeX-PDF-mode t)
+ '(TeX-view-program-list (quote (("Preview" "open -a Preview.app %o") ("Skim" "open -a Skim.app %o"))))
+ '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Skim") (output-html "xdg-open"))))
+ '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tsdh-dark)))
+ '(ess-swv-processor (quote knitr))
+ '(inhibit-startup-screen t)
+ '(show-paren-mode t)
+ '(text-mode-hook (quote (turn-on-auto-fill text-mode-hook-identify)))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "gray20" :foreground "white smoke" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "apple" :family "Monaco"))))
+ '(hl-line ((t (:inherit highlight :background "grey28"))) t))
