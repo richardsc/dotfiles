468078e778b79e22e211fe80631cfb1c7c85098c
Clark Richards <clark.richards@gmail.com>
2014-04-09 16:21:10 -0400

    enable visual bookmarks, and put bm-repository into dotfile repo

--- emacs-gnu.el
+++ emacs-gnu.el
@@ -140,3 +140,30 @@
 (autoload 'Lorem-ipsum-insert-paragraphs "lorem-ipsum" "" t)
 (autoload 'Lorem-ipsum-insert-sentences "lorem-ipsum" "" t)
 (autoload 'Lorem-ipsum-insert-list "lorem-ipsum" "" t)
+
+;; =================================================
+;; Visual bookmarks
+;; =================================================
+(setq bm-restore-repository-on-load t)
+(require 'bm)
+(global-set-key (kbd "C-c M-t") 'bm-toggle)
+(global-set-key (kbd "C-c M-n") 'bm-next)
+(global-set-key (kbd "C-c M-p") 'bm-previous)
+;; make bookmarks persistent as default
+(setq-default bm-buffer-persistence t)
+ 
+;; Loading the repository from file when on start up.
+(add-hook' after-init-hook 'bm-repository-load)
+ 
+;; Restoring bookmarks when on file find.
+(add-hook 'find-file-hooks 'bm-buffer-restore)
+ 
+;; Saving bookmark data on killing a buffer
+(add-hook 'kill-buffer-hook 'bm-buffer-save)
+ 
+;; Saving the repository to file when on exit.
+;; kill-buffer-hook is not called when emacs is killed, so we
+;; must save all bookmarks first.
+(add-hook 'kill-emacs-hook '(lambda nil
+                              (bm-buffer-save-all)
+                              (bm-repository-save)))
