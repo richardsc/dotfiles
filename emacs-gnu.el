b973cb4314b9e7a1976503d35e0519ca7a5f189f
Clark Richards <clark.richards@gmail.com>
2014-04-27 14:03:11 -0400

    trying to get ac-math working without sticking unicode in

--- emacs-gnu.el
+++ emacs-gnu.el
@@ -128,11 +128,12 @@
 
 (defun ac-latex-mode-setup ()         ; add ac-sources to default ac-sources
   (setq ac-sources
-     (append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands)
+     (append '(ac-source-math-latex ac-source-latex-commands)
                ac-sources)))
 (add-hook 'LaTeX-mode-hook 'ac-latex-mode-setup)
-(global-auto-complete-mode t)
 (ac-flyspell-workaround)
+;(global-auto-complete-mode t)
+(setq ac-math-unicode-in-math-p nil)
 
 ;; =================================================
 ;; lorem-ipsum
