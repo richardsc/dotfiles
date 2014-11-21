9258a4901acdff60a6aabd154849d0094617f10a
Clark Richards <clark.richards@gmail.com>
2014-11-21 14:00:58 -0500

    add elpa back in to access auctex

--- emacs-gnu.el
+++ emacs-gnu.el
@@ -103,8 +103,8 @@
 ;; Melpa
 ;; =================================================
 (require 'package)
-(setq package-archives '(;("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")))
 ;; (add-to-list 'package-archives
 ;;              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
