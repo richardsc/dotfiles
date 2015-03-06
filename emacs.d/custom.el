245231b0f19146289cc68ec57b1924f2205c5ca7
Clark Richards <clark.richards@gmail.com>
2015-03-06 14:40:09 -0500

    custom.el updates from matlab mode changes

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -4,17 +4,43 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(LaTeX-command "latex -synctex=1")
- '(TeX-PDF-mode t)
- '(TeX-view-program-list (quote (("Preview" "open -a Preview.app %o") ("Skim" "open -a Skim.app %o"))))
- '(TeX-view-program-selection (quote (((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi") (output-pdf "Skim") (output-html "xdg-open"))))
- '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(TeX-PDF-mode t t)
+ '(TeX-view-program-list
+   (quote
+    (("Preview" "open -a Preview.app %o")
+     ("Skim" "open -a Skim.app %o"))) t)
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and gv")
+     (output-dvi "xdvi")
+     (output-pdf "Skim")
+     (output-html "xdg-open"))) t)
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(bm-repository-file "/Users/richardsc/Dropbox/emacs/bm-repository")
  '(column-number-mode t)
  '(custom-enabled-themes (quote (tsdh-dark)))
- '(custom-safe-themes (quote ("cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" default)))
- '(ess-R-font-lock-keywords (quote ((ess-R-fl-keyword:modifiers . t) (ess-R-fl-keyword:fun-defs . t) (ess-R-fl-keyword:keywords . t) (ess-R-fl-keyword:assign-ops . t) (ess-R-fl-keyword:constants . t) (ess-fl-keyword:fun-calls . t) (ess-fl-keyword:numbers) (ess-fl-keyword:operators) (ess-fl-keyword:delimiters . t) (ess-fl-keyword:= . t) (ess-R-fl-keyword:F&T))))
+ '(custom-safe-themes
+   (quote
+    ("cd70962b469931807533f5ab78293e901253f5eeb133a46c2965359f23bfb2ea" default)))
+ '(ess-R-font-lock-keywords
+   (quote
+    ((ess-R-fl-keyword:modifiers . t)
+     (ess-R-fl-keyword:fun-defs . t)
+     (ess-R-fl-keyword:keywords . t)
+     (ess-R-fl-keyword:assign-ops . t)
+     (ess-R-fl-keyword:constants . t)
+     (ess-fl-keyword:fun-calls . t)
+     (ess-fl-keyword:numbers)
+     (ess-fl-keyword:operators)
+     (ess-fl-keyword:delimiters . t)
+     (ess-fl-keyword:= . t)
+     (ess-R-fl-keyword:F&T))))
  '(ess-swv-processor (quote knitr))
  '(inhibit-startup-screen t)
+ '(matlab-auto-fill nil)
+ '(matlab-comment-region-s "% ")
  '(show-paren-mode t)
  '(text-mode-hook (quote (text-mode-hook-identify)))
  '(tool-bar-mode nil))
