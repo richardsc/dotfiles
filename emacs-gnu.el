d6c041df486a1a57773d2449dbec31b49830b9bd
Clark Richards <clark.richards@gmail.com>
2015-03-18 09:47:42 -0400

    undo tree mode

--- emacs-gnu.el
+++ emacs-gnu.el
@@ -50,6 +50,15 @@
 (global-set-key [(control c)(f)] 'fill-region)
 (global-set-key [(control c)(F)] 'unfill-region-or-buffer)
 
+
+;; ======================================================================
+;; undo tree mode
+;; ======================================================================
+(require 'undo-tree)
+(global-undo-tree-mode)
+(setq undo-tree-visualizer-timestamps t)
+(setq undo-tree-visualizer-diff t)
+
 ;; ======================================================================
 ;; Org mode
 ;; ======================================================================
