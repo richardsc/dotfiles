58f4dcf09fda37724ceee89b82ef3730f9fabe75
Clark Richards <clark.richards@gmail.com>
2022-05-10 11:48:06 -0300

    trying to get Rnw polymode working

--- emacs-gnu.el
+++ emacs-gnu.el
@@ -184,7 +184,7 @@
 ;;; MARKDOWN
 (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
 
-;;; R modes
+;; ;;; R modes
 (add-to-list 'auto-mode-alist '("\\.Snw" . poly-noweb+r-mode))
 (add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
 (add-to-list 'auto-mode-alist '("\\.Rmd" . poly-markdown+r-mode))
